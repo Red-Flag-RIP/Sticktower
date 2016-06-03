421f1d2c5d1c48d9dc7dee7126e45fbe7cd0a3fb
SEBASTIAN <sebastian@localhost.localdomain>
2016-06-03 17:55:22 -0500

    changes

--- game.py
+++ game.py
@@ -25,6 +25,10 @@ class walls(pygame.sprite.Sprite):
 		self.rect.x=p[0]
 		self.rect.y=p[1]
 class level(object):
+	#move back
+	move_y=0
+	move_x=0
+	
 	wall=None
 	enemies_list=None
 	object_list=None
@@ -48,15 +52,24 @@ class level(object):
 		self.enemies_list.update()
 		self.object_list.update()
 		self.plataform_list.update()
+	
+	def move_back_y(self,d):
+		self.move_y=d
+		for w in self.wall:
+			w.rect.y+=d
+		self.move_y=0
 
-	def draw(self,window):
+	def draw(self,window,d):
+		self.move_y=d
 		window.fill(white)
-		window.blit(self.background,(-504,-504))
-		window.blit(self.struct,(0,-height))
+		window.blit(self.background,(-504+self.move_x,-504+self.move_y))
+		window.blit(self.struct,(0+self.move_x,-height+self.move_y))
 		self.wall.draw(window)
 		self.enemies_list.draw(window)
 		self.object_list.draw(window)
 		self.plataform_list.draw(window)
+	
+		
 	
 
 class level1(level):
@@ -64,7 +77,9 @@ class level1(level):
 	wll=[ [36*6, 36, 36*7 , height-108],
 	      [36*5, 36, 0, height-36*6],
 	      [36*2, 36, 36*9, height-36*6],
-	      [36*5, 36, 36*15, height-36*6]
+	      [36*5, 36, 36*15, height-36*6],
+	      [36*4, 36, 36*16, height-36*11],
+	      [36*6, 36, 0, height-36*12]
 	    ]
 	def __init__(self):
 		level.__init__(self)
@@ -78,27 +93,60 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self,imagen):
 		pygame.sprite.Sprite.__init__(self)
 		#apariencia del jugador
-		pygame.sprite.Sprite.__init__(self)
-		self.image=pygame.image.load(imagen).convert_alpha()
+		##self.image=pygame.image.load(imagen).convert_alpha()
+		self.image=pygame.Surface(imagen)
 		self.rect=self.image.get_rect()
 		self.rect.x=48
 		self.rect.y=height-self.rect.height
-	def update(self):
-		self.rect.x+=self.movx		
-		
+	
+	def gravity(self):
+		if self.movy==0:
+			self.movy=1
+		else:
+			self.movy+=.35
+		if self.rect.y >= height-self.rect.height and self.movy >=0:
+			self.movy=0
+			self.rect.y = height - self.rect.height
+	def jump(self):
+		self.rect.y += 2
 		collition_ls=pygame.sprite.spritecollide(self,self.level.wall,False)
+		self.rect.y -= 2
+        
+		# Si es posible saltar, aumentamos velocidad hacia arriba
+		if len(collition_ls) > 0 or self.rect.bottom >= height:
+			self.movy = -12
+	def update(self):
+		self.gravity()
+		self.rect.x+=self.movx		
+		collition_ls=pygame.sprite.spritecollide(self,self.level.wall,False)
+
+		#colision por los lados
 		for muro in collition_ls:
             		if self.movx > 0:
 				self.rect.right = muro.rect.left
 			elif self.movx < 0:
 				self.rect.left = muro.rect.right
+		#colision superior/inferior
+		self.rect.y+=self.movy
+		collition_ls=pygame.sprite.spritecollide(self,self.level.wall,False)
+		for muro in collition_ls:
+            		if self.movy > 0:
+				self.rect.bottom = muro.rect.top
+				self.movy=0
+			elif self.movy < 0:
+				self.rect.top = muro.rect.bottom
+			self.movy=0
 if __name__ == '__main__':
 	pygame.init()
 	window=pygame.display.set_mode([width,height])
 	pygame.display.set_caption("Stick tower")
+
+	#VARIABLE D QUE ME DEFINE LA VELOCIDAD DE CAMBIO DEL FONDO
+	d=0
 	
 	#jugador
-	player=Player('player.png')
+	#player=Player('player.png')
+	player=Player([36,36])
 
 	#creando niveles
 	level_list=[]
@@ -116,7 +164,7 @@ if __name__ == '__main__':
 	active_ls.add(player)
 
 	#dibujos
-	actual_level.draw(window)
+	actual_level.draw(window,d)
 	active_ls.draw(window)
 
 	end=False
@@ -133,15 +181,23 @@ if __name__ == '__main__':
 					player.movx=6
 				if event.key==pygame.K_LEFT:
 					player.movx=-6
+				if event.key==pygame.K_SPACE:
+					player.jump()
+
 
 			elif event.type==pygame.KEYUP:
 				if event.key==pygame.K_RIGHT:
 					player.movx=0
 				if event.key==pygame.K_LEFT:
 					player.movx=0
+	
+		if player.rect.y <= height/8:
+			d=10
+			actual_level.move_back_y(d)	
+		
 		actual_level.update()
 		active_ls.update()
-		actual_level.draw(window)
+		actual_level.draw(window,d)
 		active_ls.draw(window)
 		clock.tick(60)
 		pygame.display.flip()			
